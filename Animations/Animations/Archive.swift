88aa2c283575811b91991584366a129a96eedb47
narivo <jikigemer@gmail.com>
2022-11-02 02:18:33 +0800

    Day 33 – Project 6, part two

--- Animations/Animations/Archive.swift
+++ Animations/Animations/Archive.swift
@@ -7,24 +7,94 @@
 
 import SwiftUI
 
+
 struct Archive: View {
-    @State private var animationAmount = 0.0
-    //@State private var animationAmount = 1.0
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-            //withAnimation {
-                animationAmount += 360
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
             }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+/*@State private var isShowingRed = false
+
+var body: some View {
+    VStack {
+        Button("Tap Me") {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+
+        if isShowingRed {
+            Rectangle()
+                .fill(.red)
+                .frame(width: 200, height: 200)
+                //.transition(.scale)
+                .transition(.asymmetric(insertion: .scale, removal: .opacity))
+        }
+    }
+}*/
         
-        /*VStack {
+/*
+ @State private var dragAmount = CGSize.zero
+ 
+ var body: some View {
+         LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    //.onEnded { _ in dragAmount = .zero }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            //.animation(.spring(), value: dragAmount)
+ }*/
+        
+/*
+@State private var enabled = false
+ 
+var body: some View {
+         Button("Tap Me") {
+            enabled.toggle()
+        }
+        .padding(50)
+        .background(enabled ? .blue : .red)
+        .animation(nil, value: enabled) // bypass animation by passing nil
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+ }*/
+    
+/*
+ @State private var animationAmount = 0.0
+ 
+ var body: some View {
+ VStack {
             Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
             
             Spacer()
@@ -37,9 +107,14 @@ struct Archive: View {
             .foregroundColor(.white)
             .clipShape(Circle())
             .scaleEffect(animationAmount)
-        }*/
+        }
+ }*/
         
-        /*Button("Tap Me") {
+/*
+@State private var animationAmount = 1.0
+         
+var body: some View {
+         Button("Tap Me") {
             animationAmount += 1
         }
         .padding(50)
@@ -59,16 +134,27 @@ struct Archive: View {
         )
         .onAppear {
             animationAmount = 2
-        }*/
+        }
+}*/
         
-        /*.scaleEffect(animationAmount)
-        .animation(
-            .interpolatingSpring(stiffness: 50, damping: 1)
-                .delay(1)
-                .repeatCount(3, autoreverses: true),
-            value: animationAmount)*/
+/*@State private var animationAmount = 1.0
+         
+var body: some View {
+    Button("Tap Me") {
+        animationAmount += 1
     }
-}
+    .padding(50)
+    .background(.red)
+    .foregroundColor(.white)
+    .clipShape(Circle())
+    .scaleEffect(animationAmount)
+    .animation(
+        .interpolatingSpring(stiffness: 50, damping: 1)
+        .delay(1)
+        .repeatCount(3, autoreverses: true),
+        value: animationAmount
+    )
+ }*/
 
 struct Archive_Previews: PreviewProvider {
     static var previews: some View {
--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,25 +7,49 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var animationAmount = 0.0
-    //@State private var animationAmount = 1.0
-    
-    var body: some View {
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                //withAnimation {
-                animationAmount += 360
-            }
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
     }
 }
 
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
