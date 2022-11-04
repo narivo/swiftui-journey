7201b902c17ddce1f2a374bb155ea7e4a311429c
narivo <jikigemer@gmail.com>
2022-11-05 00:46:52 +0800

    try to resolve issue

--- challenge2/challenge2/ContentView.swift
+++ challenge2/challenge2/ContentView.swift
@@ -12,7 +12,20 @@ struct Question: Hashable {
     var answer: Int
 }
 
+struct QuestionGenerator {
+    var questions = [Question]()
+    
+    init(numberOfQuestions: Int, multiplier: Int) {
+        for _ in 1...numberOfQuestions {
+            let x = Int.random(in: 1...12)
+            questions.append(Question(title: "What is \(x) x \(multiplier) ?", answer: x * multiplier))
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var makeSettings = true
+    
     @State private var multiplier = 2
     @State private var numberOfQuestions = 5
     @State private var currentIndex = 1
@@ -20,37 +33,41 @@ struct ContentView: View {
     
     let availNumQuestion = [5, 10, 20]
     
-    var questions: [Question] {
-        var temp = [Question]()
-        for i in 1...numberOfQuestions {
-            let x = Int.random(in: 1...12)
-            temp.append(Question(title: "What is \(x) x \(multiplier) ?", answer: i * multiplier))
-        }
-        return temp
-    }
+    @State private var questionGenerator = QuestionGenerator(numberOfQuestions: numberOfQuestions, multiplier: multiplier)
     
     var body: some View {
-        VStack {
+        NavigationView {
             Form {
-                Section("configuration") {
-                    Stepper("Multiplier: \(multiplier)",
-                            value: $multiplier,
-                            in: 2...12)
-                    Picker("Number of questions: ", selection: $numberOfQuestions) {
-                        ForEach(availNumQuestion, id: \.self) {
-                            Text("\($0)")
+                if makeSettings {
+                    
+                    Section("configuration") {
+                        Stepper("Multiplier: \(multiplier)",
+                                value: $multiplier,
+                                in: 2...12)
+                        Picker("Number of questions: ", selection: $numberOfQuestions) {
+                            ForEach(availNumQuestion, id: \.self) {
+                                Text("\($0)")
+                            }
                         }
+                        Button("Done") {
+                            makeSettings = false
+                        }
+                    }
+                    
+                } else {
+                    
+                    Section("questions") {
+                        
+                        Text(questions[currentIndex].title)
+                        
+                        TextField("Your input", text: $userInput)
+                            .keyboardType(.numberPad)
                     }
                 }
                 
-                Text(questions[currentIndex].title)
-                
-                TextField("Your input", text: $userInput)
-                    .keyboardType(.numberPad)
             }
-            .onSubmit {
-                print("xyz")
-            }
+            .navigationTitle("Edutainment app")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItemGroup (placement: .keyboard, content: {
                     Spacer()
